4ab7ba0795cfad2a54dc0add26c1c3b593df9c2d
Michael Bluman <reasonsbluman@gmail.com>
2017-10-27 05:36:21 +0100

    Init Deliver, Upgrade to Python 3.6.2

---
+++ jollof/deliver.py
@@ -0,0 +1,16 @@
+import os
+import re
+import requests
+import json
+import random
+import string
+import geopy.distance
+from pprint import pprint
+
+from jollof.models import *
+
+
+class Buy(object):
+
+    def __init__(self):
+        pass
